f7391281d7f4499fe27835e91fa8404bd48dedff
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-04 18:29:22 +0000

    First commit

---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,8 @@
+feature 'Viewing bookmarks' do
+  scenario 'A user can see bookmarks' do
+    visit('/bookmarks')
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
+  end
+end
