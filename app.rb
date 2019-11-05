778acfbd29a7b2a0e711b3f06eac1d4d2b711489
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-05 21:52:27 +0000

    Refactored to use Classes.

--- app.rb
+++ app.rb
@@ -1,13 +1,12 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    "Hello World!"
-  end
+
 
   get '/bookmarks' do
-    erb :bookmarks
-
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
   end
 
 run! if app_file == $0
