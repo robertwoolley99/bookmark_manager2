d188d306e692289b6364b6924b746e95949518ab
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-06 14:29:14 +0000

    Integration with postgresql added.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,9 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-    ]
+  connection = PG.connect(dbname: 'bookmark_manager')
+  result = connection.exec("SELECT * FROM bookmarks;")
+  result.map { |bookmarks| bookmarks['url'] }
   end
 end
